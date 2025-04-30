7446e3599b342dd896ac393636bc0f6e27bd4caf
Phil Evans <phil.evans@tower.co.nz>
2025-04-30 12:02:09 +1200

    simple chat code using openai and inference

---
+++ AISkills/AgenticSolutions/src/inference-chat/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Azure;
+using Azure.AI.Projects;
+using Azure.Identity;
+using Azure.AI.Inference;
+
+namespace my_foundry_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Initialize the project client
+                var connectionString = "australiaeast.api.azureml.ms;1ef96a3c-ccac-4237-a697-f11527a50fc7;npd-mywave-rg;mywave-learning";
+                var projectClient = new AIProjectClient(connectionString, new DefaultAzureCredential());
+
+                // Get a chat client
+                ChatCompletionsClient chatClient = projectClient.GetChatCompletionsClient();
+
+                // Initialize prompt with system message
+                var prompt = new List<ChatRequestMessage>(){
+                    new ChatRequestSystemMessage("You are a helpful AI assistant that answers questions.")
+                };
+
+                // Get a chat completion based on a user-provided prompt
+                Console.WriteLine("Enter a question:");
+                var user_prompt = Console.ReadLine();
+
+                prompt.Add(new ChatRequestUserMessage(user_prompt));
+
+                var requestOptions = new ChatCompletionsOptions()
+                {
+                    Model = "gpt-4o",
+                    Messages = prompt
+                };
+
+                Response<ChatCompletions> response = chatClient.Complete(requestOptions);
+                Console.WriteLine(response.Value.Content);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ AISkills/AgenticSolutions/src/openai-chat/Program.cs
@@ -0,0 +1,45 @@
+﻿//using System;
+//using Azure;
+using Azure.Identity;
+using OpenAI.Chat;
+using Azure.AI.OpenAI;
+
+// Note you need to assign Cognitive Services User to your user in the AI project resource group for this to work
+
+namespace my_foundry_client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var deploymentName = "gpt-4o";
+                var endpointUrl = "https://ai-npdmywave283959875793.openai.azure.com/";
+
+                var client = new AzureOpenAIClient(new Uri(endpointUrl), new DefaultAzureCredential());
+                var chatClient = client.GetChatClient(deploymentName);
+
+                Console.WriteLine("Enter a question:");
+                var user_prompt = Console.ReadLine();
+
+                var messages = new List<ChatMessage>();
+                messages.Add(new SystemChatMessage("You are a helpful assistant that answers questions."));
+                messages.Add(new UserChatMessage(user_prompt));
+
+                var response = await chatClient.CompleteChatAsync(messages,  new ChatCompletionOptions()
+                    {
+                        Temperature = (float)0.7,
+                        FrequencyPenalty = (float)0,
+                        PresencePenalty = (float)0,
+                    });
+                var chatResponse = response.Value.Content.Last().Text;
+                Console.WriteLine(chatResponse);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
